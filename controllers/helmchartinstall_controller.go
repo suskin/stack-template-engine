9405a10edbee7668fee9435cfb7c1ee553016605
suskin <813571+suskin@users.noreply.github.com>
2019-11-04 17:28:08 -0800

    project: initial commit
    This is the first commit of the project, and was generated using
    kubebuilder 2.0.1 and the crossplane cli 0.2.0, by following the
    crossplane cli quick start.
    
    Signed-off-by: suskin <813571+suskin@users.noreply.github.com>

---
+++ controllers/helmchartinstall_controller.go
@@ -0,0 +1,50 @@
+/*
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllers
+
+import (
+	"context"
+
+	"github.com/go-logr/logr"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	helmv1alpha1 "github.com/suskin/stack-helm/api/v1alpha1"
+)
+
+// HelmChartInstallReconciler reconciles a HelmChartInstall object
+type HelmChartInstallReconciler struct {
+	client.Client
+	Log logr.Logger
+}
+
+// +kubebuilder:rbac:groups=helm.samples.stacks.crossplane.io,resources=helmchartinstalls,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=helm.samples.stacks.crossplane.io,resources=helmchartinstalls/status,verbs=get;update;patch
+
+func (r *HelmChartInstallReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
+	_ = context.Background()
+	_ = r.Log.WithValues("helmchartinstall", req.NamespacedName)
+
+	// your logic here
+
+	return ctrl.Result{}, nil
+}
+
+func (r *HelmChartInstallReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&helmv1alpha1.HelmChartInstall{}).
+		Complete(r)
+}
