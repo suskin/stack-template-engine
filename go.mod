0eac13488b867a9544195d9b4c5f2c147b573809
suskin <813571+suskin@users.noreply.github.com>
2019-11-04 18:20:32 -0800

    helm: add helm rendering code from gitlab-controller
    This copies the helm-rendering code from the gitlab controller codebase
    (it doesn't build yet though).
    
    Signed-off-by: suskin <813571+suskin@users.noreply.github.com>

--- go.mod
+++ go.mod
@@ -3,10 +3,21 @@ module github.com/suskin/stack-helm
 go 1.12
 
 require (
+	github.com/BurntSushi/toml v0.3.1 // indirect
+	github.com/Masterminds/semver v1.5.0 // indirect
+	github.com/crossplaneio/gitlab-controller v0.0.0-20190520215645-90e2a0ea1ff1
+	github.com/cyphar/filepath-securejoin v0.2.2 // indirect
+	github.com/ghodss/yaml v1.0.0 // indirect
 	github.com/go-logr/logr v0.1.0
-	github.com/onsi/ginkgo v1.6.0
-	github.com/onsi/gomega v1.4.2
+	github.com/gobwas/glob v0.2.3 // indirect
+	github.com/google/go-cmp v0.3.0
+	github.com/onsi/ginkgo v1.8.0
+	github.com/onsi/gomega v1.5.0
+	github.com/pkg/errors v0.8.1
+	k8s.io/api v0.0.0-20190409021203-6e4e0e4f393b
 	k8s.io/apimachinery v0.0.0-20190404173353-6a84e37a896d
 	k8s.io/client-go v11.0.1-0.20190409021438-1a26190bd76a+incompatible
+	k8s.io/helm v2.15.2+incompatible
 	sigs.k8s.io/controller-runtime v0.2.2
+	sigs.k8s.io/controller-tools v0.2.1 // indirect
 )
---
+++ pkg/helm/helm_test.go
@@ -0,0 +1,319 @@
+/*
+Copyright 2019 The GitLab-Controller Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package helm
+
+/*
+import (
+	"bytes"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"path/filepath"
+	"reflect"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/pkg/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/helm/pkg/chartutil"
+)
+
+const (
+	name      = "cool"
+	namespace = "coolns"
+
+	chartName = "simple"
+	chartDir  = "fixtures/" + chartName
+	chartPath = "/" + chartName + "-1.0.0.tgz"
+)
+
+func TestProduce(t *testing.T) {
+	type want struct {
+		r   Resources
+		err error
+	}
+	cases := []struct {
+		name     string
+		chartDir string
+		opts     []Option
+		want     want
+	}{
+		{
+			name:     "IngressEnabled",
+			chartDir: chartDir,
+			opts: []Option{
+				WithValues(chartutil.Values{
+					// The ingress is disabled by default.
+					"ingress": map[string]bool{"enabled": true},
+				}),
+			},
+			want: want{
+				r: Resources{
+					{
+						Object: map[string]interface{}{
+							"apiVersion": "v1",
+							"kind":       "Service",
+							"metadata":   map[string]interface{}{"name": chartName},
+							"spec": map[string]interface{}{
+								"ports": []interface{}{
+									map[string]interface{}{
+										"name":       "http",
+										"port":       int64(80),
+										"protocol":   "TCP",
+										"targetPort": "http",
+									},
+								},
+								"selector": map[string]interface{}{
+									"app.kubernetes.io/instance": chartName,
+									"app.kubernetes.io/name":     chartName,
+								},
+								"type": "ClusterIP",
+							},
+						},
+					},
+					{
+						Object: map[string]interface{}{
+							"apiVersion": "extensions/v1beta1",
+							"kind":       "Ingress",
+							"metadata":   map[string]interface{}{"name": chartName},
+							"spec": map[string]interface{}{
+								"rules": []interface{}{
+									map[string]interface{}{"host": "chart-example.local"},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			name:     "IngressDisabled",
+			chartDir: chartDir,
+			// The ingress is disabled by default, so we omit values.
+			want: want{
+				r: Resources{
+					{
+						Object: map[string]interface{}{
+							"apiVersion": "v1",
+							"kind":       "Service",
+							"metadata":   map[string]interface{}{"name": chartName},
+							"spec": map[string]interface{}{
+								"ports": []interface{}{
+									map[string]interface{}{
+										"name":       "http",
+										"port":       int64(80),
+										"protocol":   "TCP",
+										"targetPort": "http",
+									},
+								},
+								"selector": map[string]interface{}{
+									"app.kubernetes.io/instance": chartName,
+									"app.kubernetes.io/name":     chartName,
+								},
+								"type": "ClusterIP",
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			name:     "WithReleaseName",
+			chartDir: chartDir,
+			opts:     []Option{WithReleaseName(name)},
+			want: want{
+				r: Resources{
+					{
+						Object: map[string]interface{}{
+							"apiVersion": "v1",
+							"kind":       "Service",
+							"metadata":   map[string]interface{}{"name": name + "-" + chartName},
+							"spec": map[string]interface{}{
+								"ports": []interface{}{
+									map[string]interface{}{
+										"name":       "http",
+										"port":       int64(80),
+										"protocol":   "TCP",
+										"targetPort": "http",
+									},
+								},
+								"selector": map[string]interface{}{
+									"app.kubernetes.io/instance": name,
+									"app.kubernetes.io/name":     chartName,
+								},
+								"type": "ClusterIP",
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			name:     "WithDefaultNamespace",
+			chartDir: chartDir,
+			opts:     []Option{WithValues(chartutil.Values{"setNamespace": true})},
+			want: want{
+				r: Resources{
+					{
+						Object: map[string]interface{}{
+							"apiVersion": "v1",
+							"kind":       "Service",
+							"metadata": map[string]interface{}{
+								"name":      chartName,
+								"namespace": metav1.NamespaceDefault,
+							},
+							"spec": map[string]interface{}{
+								"ports": []interface{}{
+									map[string]interface{}{
+										"name":       "http",
+										"port":       int64(80),
+										"protocol":   "TCP",
+										"targetPort": "http",
+									},
+								},
+								"selector": map[string]interface{}{
+									"app.kubernetes.io/instance": chartName,
+									"app.kubernetes.io/name":     chartName,
+								},
+								"type": "ClusterIP",
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			name:     "WithSpecifiedNamespace",
+			chartDir: chartDir,
+			opts: []Option{
+				WithValues(chartutil.Values{"setNamespace": true}),
+				WithReleaseNamespace(namespace),
+			},
+			want: want{
+				r: Resources{
+					{
+						Object: map[string]interface{}{
+							"apiVersion": "v1",
+							"kind":       "Service",
+							"metadata": map[string]interface{}{
+								"name":      chartName,
+								"namespace": namespace,
+							},
+							"spec": map[string]interface{}{
+								"ports": []interface{}{
+									map[string]interface{}{
+										"name":       "http",
+										"port":       int64(80),
+										"protocol":   "TCP",
+										"targetPort": "http",
+									},
+								},
+								"selector": map[string]interface{}{
+									"app.kubernetes.io/instance": chartName,
+									"app.kubernetes.io/name":     chartName,
+								},
+								"type": "ClusterIP",
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			tarball, err := targz(tc.chartDir)
+			if err != nil {
+				t.Fatalf("targz(%s): %s", tc.chartDir, err)
+			}
+
+			ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				tarball.WriteTo(w)
+				r.Body.Close()
+			}))
+			defer ts.Close()
+
+			r, err := Render(ts.URL+chartPath, tc.opts...)
+			if diff := cmp.Diff(tc.want.err, err, EquateErrors()); diff != "" {
+				t.Fatalf("Render(...): -want error, +got error: %s", diff)
+			}
+			if diff := cmp.Diff(tc.want.r, r); diff != "" {
+				t.Errorf("Render(...): -want resources, +got resources: %s", diff)
+			}
+		})
+	}
+}
+
+// targz uses Helm's chart packaging logic to package a chart directory into a
+// gzipped tarball. It writes the tarball out to a temporary file because Helm's
+// chartutil functions are hardwired to read and write to the OS filesystem.
+func targz(chartDir string) (io.WriterTo, error) {
+	path, err := filepath.Abs(chartDir)
+	if err != nil {
+		return nil, errors.Wrapf(err, "cannot determine absolute path of %s", chartDir)
+	}
+
+	c, err := chartutil.LoadDir(path)
+	if err != nil {
+		return nil, errors.Wrapf(err, "cannot load %s", path)
+	}
+
+	tmp, err := ioutil.TempDir(os.TempDir(), "chart")
+	if err != nil {
+		return nil, errors.Wrap(err, "cannot create temp dir")
+	}
+	defer os.RemoveAll(tmp)
+
+	f, err := chartutil.Save(c, tmp)
+	if err != nil {
+		return nil, errors.Wrap(err, "cannot save chart tarball")
+	}
+
+	tarball, err := os.Open(f)
+	if err != nil {
+		return nil, errors.Wrap(err, "cannot open chart tarball")
+	}
+	defer tarball.Close()
+
+	b := &bytes.Buffer{}
+	if _, err := b.ReadFrom(tarball); err != nil {
+		return nil, errors.Wrap(err, "cannot read chart tarball")
+	}
+
+	return b, nil
+}
+
+func EquateErrors() cmp.Option {
+	return cmp.Comparer(func(a, b error) bool {
+		if a == nil || b == nil {
+			return a == nil && b == nil
+		}
+
+		av := reflect.ValueOf(a)
+		bv := reflect.ValueOf(b)
+		if av.Type() != bv.Type() {
+			return false
+		}
+
+		return a.Error() == b.Error()
+	})
+}
+
+*/
