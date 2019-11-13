7300a8995d5d41f492baa658a8bfe345662143ba
suskin <813571+suskin@users.noreply.github.com>
2019-11-12 16:25:45 -0800

    configuration engine: add job creation
    Now that we have a job that will tie a stack to a configuration engine
    to kubectl, this changeset updates the controller to create such a job
    when a helm chart install object appears.
    
    Signed-off-by: suskin <813571+suskin@users.noreply.github.com>

--- controllers/helmchartinstall_controller.go
+++ controllers/helmchartinstall_controller.go
@@ -17,15 +17,21 @@ package controllers
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/go-logr/logr"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
+	batchv1 "k8s.io/api/batch/v1"
+	corev1 "k8s.io/api/core/v1"
 	kerrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 
 	helmv1alpha1 "github.com/suskin/stack-helm/api/v1alpha1"
+
+	"github.com/crossplaneio/crossplane-runtime/pkg/meta"
 )
 
 // HelmChartInstallReconciler reconciles a HelmChartInstall object
@@ -52,10 +58,12 @@ func (r *HelmChartInstallReconciler) Reconcile(req ctrl.Request) (ctrl.Result, e
 
 	r.Log.V(0).Info("Hello World!", "instanceName", req.NamespacedName, "instance", i)
 
+	r.render(ctx, i)
+
 	return ctrl.Result{}, nil
 }
 
-func (r *HelmChartInstallReconciler) render() error {
+func (r *HelmChartInstallReconciler) render(ctx context.Context, claim *helmv1alpha1.HelmChartInstall) error {
 	/**
 	* Steps to rendering:
 	- Grab the original claim
@@ -71,15 +79,12 @@ func (r *HelmChartInstallReconciler) render() error {
 	- When an instance is created, feed it into the renderer
 	*/
 
-	// claim is an unstructured.Unstructured
-	claim, err := r.getClaim()
-
 	// configuration is a typed object
 	configuration, err := r.getStackConfiguration()
 
 	// TODO the status will have more than *just* the result in it,
 	// but we can start with just the result
-	result, err := r.processConfiguration(claim, configuration)
+	result, err := r.processConfiguration(ctx, claim, configuration)
 
 	err = r.setClaimStatus(claim, result)
 
@@ -95,18 +100,154 @@ func (r *HelmChartInstallReconciler) getStackConfiguration() (unstructured.Unstr
 	// See the template stacks internal design doc for details, but
 	// the most likely source of the stack configuration is the stack object itself.
 	// Other potential sources include a configmap
+
+	// TODO
+	// - Stack configuration should be a structured object
+	// - Stack configuration will be coming from a Kubernetes object, probably the
+	//   Stack itself
+	//stackConfiguration := ''
 	return unstructured.Unstructured{}, nil
 }
 
 func (r *HelmChartInstallReconciler) processConfiguration(
-	claim unstructured.Unstructured, configuration unstructured.Unstructured,
+	ctx context.Context,
+	claim *helmv1alpha1.HelmChartInstall,
+	configuration unstructured.Unstructured,
 ) (unstructured.Unstructured, error) {
-	//job := Job{}
+	// Given a claim and a stack configuration, transform the claim into something
+	// to inject into a stack's configuration renderer, and then render the Stack's
+	// resources from the configuration and the transformed claim
+	ownerRef := meta.AsOwner(meta.ReferenceTo(claim, claim.GroupVersionKind()))
+	var jobBackoff int32
+	jobBackoff = 0
+	// TODO Handle multiple resource dirs. Perhaps try rolling everything into a single job,
+	//      so have multiple engine containers, one for each path.
+	//      And multiple apply containers; one for each path. Or, make multiple jobs.
+	// TODO we may want some of these steps to be in this controller instead of
+	//      being outsourced
+
+	// TODO resource dir will come from the stack configuration
+	targetResourceDir := "myResourceDir"
+	// TODO target stack image will come from the claim
+	targetStackImage := "crossplane/template-stack:latest"
+	resourceDirName := fmt.Sprintf("/.registry/resources/%s", targetResourceDir)
+
+	stackConfigurationVolumeName := "stack-configuration"
+	stackContentsDestinationDir := "/usr/share/input/"
+
+	resourceConfigurationVolumeName := "resource-configuration"
+	resourceConfigurationDestinationDir := "/usr/share/resource-configuration/"
+	targetNamespace := claim.GetNamespace()
+
+	job := &batchv1.Job{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "helm-template-apply",
+			Namespace: targetNamespace,
+			OwnerReferences: []metav1.OwnerReference{
+				ownerRef,
+			},
+		},
+		Spec: batchv1.JobSpec{
+			BackoffLimit: &jobBackoff,
+			Template: corev1.PodTemplateSpec{
+				Spec: corev1.PodSpec{
+					RestartPolicy: corev1.RestartPolicyNever,
+					InitContainers: []corev1.Container{
+						{
+							Name:  "load-stack",
+							Image: targetStackImage,
+							Command: []string{
+								// The "." suffix causes the cp -R to copy the contents of the directory instead of
+								// the directory itself
+								"cp", "-R", fmt.Sprintf("%s/.", resourceDirName), stackContentsDestinationDir,
+							},
+							VolumeMounts: []corev1.VolumeMount{
+								{
+									Name:      stackConfigurationVolumeName,
+									MountPath: stackContentsDestinationDir,
+								},
+							},
+							ImagePullPolicy: corev1.PullNever,
+						},
+						{
+							Name:  "engine",
+							Image: "crossplane/helm-engine:latest",
+							Command: []string{
+								"helm",
+							},
+							Args: []string{
+								"template",
+								"--output-dir", resourceConfigurationDestinationDir,
+								"--namespace", targetNamespace,
+								stackContentsDestinationDir,
+							},
+							VolumeMounts: []corev1.VolumeMount{
+								{
+									Name:      stackConfigurationVolumeName,
+									MountPath: stackContentsDestinationDir,
+								},
+								{
+									Name:      resourceConfigurationVolumeName,
+									MountPath: resourceConfigurationDestinationDir,
+								},
+							},
+							ImagePullPolicy: corev1.PullNever,
+						},
+					},
+					Containers: []corev1.Container{
+						{
+							Name:  "kubectl",
+							Image: "crossplane/kubectl:latest",
+							// "--debug" can be added to this list of Args to get debug output from the job,
+							// but note that will be included in the stdout from the pod, which makes it
+							// impossible to create the resources that the job unpacks.
+							Command: []string{
+								"kubectl",
+							},
+							Args: []string{
+								"apply",
+								"--namespace", targetNamespace,
+								"-R",
+								"-f",
+								resourceConfigurationDestinationDir,
+							},
+							VolumeMounts: []corev1.VolumeMount{
+								{
+									Name:      resourceConfigurationVolumeName,
+									MountPath: resourceConfigurationDestinationDir,
+								},
+							},
+							ImagePullPolicy: corev1.PullNever,
+						},
+					},
+					Volumes: []corev1.Volume{
+						{
+							Name: stackConfigurationVolumeName,
+							VolumeSource: corev1.VolumeSource{
+								EmptyDir: &corev1.EmptyDirVolumeSource{},
+							},
+						},
+						{
+							Name: resourceConfigurationVolumeName,
+							VolumeSource: corev1.VolumeSource{
+								EmptyDir: &corev1.EmptyDirVolumeSource{},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	if err := r.Create(ctx, job); err != nil {
+		return unstructured.Unstructured{}, err
+	}
+
 	return unstructured.Unstructured{}, nil
 }
 
 func (r *HelmChartInstallReconciler) setClaimStatus(
-	claim unstructured.Unstructured, result unstructured.Unstructured,
+	claim *helmv1alpha1.HelmChartInstall, result unstructured.Unstructured,
 ) error {
 	// The claim is the CR that triggered this whole thing.
 	// The result is the result of trying to apply or delete the templates rendered from processing the claim.
